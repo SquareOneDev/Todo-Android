c07505c8cfe93d29cab96ef8bd1a31b8b5d48b68
ohhyunjin <javieroh@hotmail.com>
2018-08-02 12:57:54 -0700

    Added RecyclerviewAdapter class and updated dependency versions on both gradle files

--- app/src/main/java/com/squareonedev/todolist/RecyclerviewAdapter.java
+++ app/src/main/java/com/squareonedev/todolist/RecyclerviewAdapter.java
@@ -1,4 +1,13 @@
 package com.squareonedev.todolist;
 
-public class RecyclerviewAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class RecyclerviewAdapter{
+
 }
